d3d608c6411961b29c3d5419714b4ea3ef4f3e40
philipwanjiku <muhorophilip7@gmail.com>
2024-01-17 15:34:31 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def is_anagram(self, word1, word2):
+        # Your anagram detection logic here
+        return sorted(word1) == sorted(word2)
+
+    def match(self, word_list):
+        matching_words = [word for word in word_list if self.is_anagram(self.word, word)]
+        return matching_words
